ab37c0e39012523b72ebdb2f16933e6c934505ae
Alexey <krugombeda@gmail.com>
2018-05-05 17:39:32 +0700

    fix date format, add ids

--- src/samples.js
+++ src/samples.js
@@ -1,23 +1,28 @@
+import {getRandom} from './helpers';
+
 const todoSamples = [
   {
+    id: getRandom(),
     title: 'Buy some bread',
     description: 'Nina loves bread!',
     importance: 'Usual',
-    date: '05.05.2018',
+    date: '2018-05-05',
     complete: false
   },
   {
+    id: getRandom(),
     title: 'Call Mom',
     description: 'Dont forget about her birthday. Need to buy a present.',
     importance: 'High priority',
-    date: '09.05.2018',
+    date: '2018-05-09',
     complete: false
   },
   {
+    id: getRandom(),
     title: 'Pay for the internet',
     description: '',
     importance: 'Usual',
-    date: '15.05.2018',
+    date: '2018-05-30',
     complete: false
   }
 ];
